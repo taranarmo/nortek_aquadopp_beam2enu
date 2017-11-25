1d9f2c1628b97efb695487b2994c288a0ba42a25
taranarmo <taranarmo@gmail.com>
2017-11-25 09:17:31 +0300

    add run time measurement to python part

--- beam2enu.sh
+++ beam2enu.sh
@@ -2,5 +2,5 @@
 
 echo "Bulding index file"
 cat "$1".sen | awk '{split($0, a, " "); print a[3]"-"a[1]"-"a[2]" "a[4]":"a[5]":"a[6]}' > "$1"_index_file
-python beam2enu.py "$1"
+time python beam2enu.py "$1"
 
