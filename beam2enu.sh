57e08cb75377986ced87da31034d3f9f9f7a899c
taranarmo <taranarmo@gmail.com>
2018-02-12 09:42:00 +0300

    add ability to work with many .hdr files in single directory

--- beam2enu.sh
+++ beam2enu.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 echo "Bulding index file"
-cat "$1".sen | awk '{split($0, a, " "); print a[3]"-"a[1]"-"a[2]" "a[4]":"a[5]":"a[6]}' > "$1"_index_file
-time python beam2enu.py "$1"
-
+if [ -z "$1" ]
+    then
+        hdr_files=`ls *.hdr`
+    else
+        hdr_files="$1.hdr"
+fi
+for hdr_file in $hdr_files
+    do
+    hdr_filename=${hdr_file%'.hdr'}
+    cat "$hdr_filename".sen | awk '{split($0, a, " "); print a[3]"-"a[1]"-"a[2]" "a[4]":"a[5]":"a[6]}' > "$hdr_filename"_index_file
+    time python beam2enu.py "$hdr_filename"
+done
